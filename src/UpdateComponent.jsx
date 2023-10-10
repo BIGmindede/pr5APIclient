94eb1665cd3ce1ea689d88fd6aae91d6f78507dc
BIGmindede <72280742+BIGmindede@users.noreply.github.com>
2023-10-10 11:08:02 +0300

    Update UpdateComponent.jsx

--- src/UpdateComponent.jsx
+++ src/UpdateComponent.jsx
@@ -29,8 +29,7 @@ function UpdateComponent() {
     }
 
     return (
-        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
-            <div className="w-50 bg-white rounded p-3">
+        <div className="d-flex vh-100 bg-dark justify-content-center align-items-center">
                 <form onSubmit={Update}>
                     <h2>Update component</h2>
                     <div className="mb-2">
@@ -53,7 +52,6 @@ function UpdateComponent() {
                     </div>
                     <button className="btn btn-success">Update</button>
                 </form>
-            </div>
         </div>
     )
 }
